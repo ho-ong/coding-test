879a0e82fa1fa16c35a791dd4d3da0bfb950bdc0
ho-ong <honghj8959@gmail.com>
2023-10-08 01:08:06 +0900

    Update Programmers Code (Java)

--- Programmers/Java/Lv.0/[Lv.0] 중앙값 구하기/[Lv.0] 중앙값 구하기_00.java
+++ Programmers/Java/Lv.0/[Lv.0] 중앙값 구하기/[Lv.0] 중앙값 구하기_00.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] array) {
         Arrays.sort(array);
-        // 배열의 길이를 2로 나누면 중앙값
         return array[array.length / 2];
     }
 }
