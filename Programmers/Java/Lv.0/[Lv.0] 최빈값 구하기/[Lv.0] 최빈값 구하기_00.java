f2dbc97cb0842f927418cb4f85804b9d3fbc036f
ho-ong <honghj8959@gmail.com>
2023-10-10 11:38:52 +0900

    Update Programmers Code (Java)

--- Programmers/Java/Lv.0/[Lv.0] 최빈값 구하기/[Lv.0] 최빈값 구하기_00.java
+++ Programmers/Java/Lv.0/[Lv.0] 최빈값 구하기/[Lv.0] 최빈값 구하기_00.java
@@ -5,14 +5,9 @@ class Solution {
         Arrays.sort(array);
 
         int answer = 0;
-
-        // 최댓값
-        int max = 0;
+        int max = 0; // 최댓값
         max = array[array.length - 1];
-
-        // count 구하기
-        // 배열의 인덱스는 0부터 시작하므로 + 1
-        int count[] = new int[max + 1];
+        int count[] = new int[max + 1]; // 빈도수
 
         // 배열의 원소가 같은지 체크
         for (int i = 0; i < array.length; i++) {
@@ -20,13 +15,12 @@ class Solution {
             count[array[i]]++;
         }
 
-        // count[0(max), 1, 2, 3, ...]
         max = count[0];
 
         // 최빈값 구하기
         for (int i = 1; i < count.length; i++) {
             if (max < count[i]) {
-                // 최빈값의 인덱스
+                // 최빈값이 1개면 i
                 max = count[i];
                 answer = i;
             } else if (max == count[i]) {
--- Programmers/Java/Lv.0/[Lv.0] 최빈값 구하기/[Lv.0] 최빈값 구하기_01.java
+++ Programmers/Java/Lv.0/[Lv.0] 최빈값 구하기/[Lv.0] 최빈값 구하기_01.java
@@ -3,7 +3,18 @@ import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] array) {
-        List<Map.Entry<Integer, List<Integer>>> list = new ArrayList<>(Arrays.stream(array).boxed().collect(Collectors.groupingBy(o -> o)).entrySet()).stream().sorted((t0, t1) -> Integer.compare(t1.getValue().size(), t0.getValue().size())).collect(Collectors.toList());
+        List<Map.Entry<Integer, List<Integer>>> list = new ArrayList<>(
+            // Collectors.groupingBy : 배열의 원소를 그룹화
+            // entrySet() : Map의 Key-Value 집합 반환
+            Arrays.stream(array).boxed().collect(Collectors.groupingBy(o -> o)).entrySet())
+            // 리스트 -> 스트림 변환
+            // Integer.compare() : 정렬 순서 결정
+            .stream().sorted((t0, t1) -> Integer.compare(t1.getValue().size(), t0.getValue().size()))
+            // 스트림 -> 리스트 변환
+            .collect(Collectors.toList());
+
+        // list의 크기가 1보다 크고, list의 1, 2번째 값의 차가 0이면 -1
+        // 아니면 list의 1번째 키 반환
         return list.size() > 1 && list.get(0).getValue().size() - list.get(1).getValue().size() == 0 ? -1 : list.get(0).getKey();
     }
 }
