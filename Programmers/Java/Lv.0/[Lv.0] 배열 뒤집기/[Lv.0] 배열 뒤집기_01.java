85ed883a3add3e83fb99fb348bc93f2c1ac19544
ho-ong <honghj8959@gmail.com>
2023-10-11 13:26:25 +0900

    Update Programmers Code (Java)

--- Programmers/Java/Lv.0/[Lv.0] 배열 뒤집기/[Lv.0] 배열 뒤집기_01.java
+++ Programmers/Java/Lv.0/[Lv.0] 배열 뒤집기/[Lv.0] 배열 뒤집기_01.java
@@ -6,7 +6,7 @@ class Solution {
         // reverse() : 원소의 반전 반환
         // mapToInt() : 기본 타입 스트림인 IntStream으로 변환
         // 기본 타입(Primitive Type) 스트림 : IntStream, LongStream, DoubleStream
-        List<Integer> list = Arrays.stream(numList).boxed().collect(Collectors.toList());
+        List<Integer> list = Arrays.stream(num_list).boxed().collect(Collectors.toList());
         Collections.reverse(list);
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
