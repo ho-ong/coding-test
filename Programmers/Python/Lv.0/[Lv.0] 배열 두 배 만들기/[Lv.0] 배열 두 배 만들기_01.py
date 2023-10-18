f2507c85af1d2541808a242839ff6fef35908a14
ho-ong <honghj8959@gmail.com>
2023-10-18 10:31:02 +0900

    Add Programmers Code (Python)

---
+++ Programmers/Python/Lv.0/[Lv.0] 배열 두 배 만들기/[Lv.0] 배열 두 배 만들기_01.py
@@ -0,0 +1,3 @@
+def solution(numbers):
+    # 배열의 원소에 2를 곱하기
+    return [i * 2 for i in numbers]
---
+++ Programmers/Python/Lv.0/[Lv.0] 숫자 비교하기/[Lv.0] 숫자 비교하기_01.py
@@ -0,0 +1,3 @@
+def solution(num1, num2):
+    # num1과 num2가 같으면 1, 다르면 -1
+    return 1 if num1 == num2 else -1
