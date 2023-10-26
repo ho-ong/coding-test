eb7a66d26a347b967bcf43892c47ebcecd16e909
ho-ong <honghj8959@gmail.com>
2023-10-26 12:47:28 +0900

    Update Programmers Code (Java)

--- Programmers/Java/Lv.0/[Lv.0] 짝수의 합/[Lv.0] 짝수의 합_00.java
+++ Programmers/Java/Lv.0/[Lv.0] 짝수의 합/[Lv.0] 짝수의 합_00.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        // 홀수 구하기 (2부터 시작, 2씩 증가)
+        // 짝수 구하기 (2부터 시작, 2씩 증가)
         for (int i = 2; i <= n; i += 2) {
             // 짝수를 모두 더하기
             answer += i;
--- Programmers/Java/Lv.0/[Lv.0] 짝수의 합/[Lv.0] 짝수의 합_01.java
+++ Programmers/Java/Lv.0/[Lv.0] 짝수의 합/[Lv.0] 짝수의 합_01.java
@@ -2,7 +2,7 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int n) {
-        // 홀수 구하기 (홀수는 2로 나눌 경우, 나머지가 1)
+        // 짝수 구하기 (짝수는 2로 나눌 경우, 나머지가 0)
         return IntStream.rangeClosed(0, n).filter(value -> value % 2 == 0).sum();
     }
 }
