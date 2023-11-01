12ea38bc2309eb8ff6544015c53c3c5af5a99896
ho-ong <honghj8959@gmail.com>
2023-11-01 10:59:37 +0900

    Add Programmers Code (Java)

---
+++ Programmers/Java/Lv.0/[Lv.0] 모스부호 (1)/[Lv.0] 모스부호 (1)_00.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String letter) {
+        // map 생성 (키, 값 저장)
+        Map<String, String> map = new HashMap<>() {{
+            put(".-", "a");
+            put("-...", "b");
+            put("-.-.", "c");
+            put("-..", "d");
+            put(".", "e");
+            put("..-.", "f");
+            put("--.", "g");
+            put("....", "h");
+            put("..", "i");
+            put(".---", "j");
+            put("-.-", "k");
+            put(".-..", "l");
+            put("--", "m");
+            put("-.", "n");
+            put("---", "o");
+            put(".--.", "p");
+            put("--.-", "q");
+            put(".-.", "r");
+            put("...", "s");
+            put("-", "t");
+            put("..-", "u");
+            put("...-", "v");
+            put(".--", "w");
+            put("-..-", "x");
+            put("-.--", "y");
+            put("--..", "z");
+        }};
+
+        String answer = "";
+        String[] str = letter.split(" ");
+
+        for (String morse : str) {
+            // map의 값을 가져와 합치기
+            answer += map.get(morse);
+        }
+
+        return answer;
+    }
+}
---
+++ Programmers/Java/Lv.0/[Lv.0] 모스부호 (1)/[Lv.0] 모스부호 (1)_01.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String letter) {
+        // map 생성 (키, 값 저장)
+        Map<String, String> map = new HashMap<>() {{
+            put(".-", "a");
+            put("-...", "b");
+            put("-.-.", "c");
+            put("-..", "d");
+            put(".", "e");
+            put("..-.", "f");
+            put("--.", "g");
+            put("....", "h");
+            put("..", "i");
+            put(".---", "j");
+            put("-.-", "k");
+            put(".-..", "l");
+            put("--", "m");
+            put("-.", "n");
+            put("---", "o");
+            put(".--.", "p");
+            put("--.-", "q");
+            put(".-.", "r");
+            put("...", "s");
+            put("-", "t");
+            put("..-", "u");
+            put("...-", "v");
+            put(".--", "w");
+            put("-..-", "x");
+            put("-.--", "y");
+            put("--..", "z");
+        }};
+
+        // map의 값을 가져와 합치기
+        return Arrays.stream(letter.split(" ")).map(map::get).collect(Collectors.joining());
+    }
+}
