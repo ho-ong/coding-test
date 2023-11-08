cb0f2f793479f8706640038de695cfab179fc141
ho-ong <honghj8959@gmail.com>
2023-11-08 11:35:37 +0900

    Update Programmers Code (Python)

--- Programmers/Python/Lv.0/[Lv.0] 짝수 홀수 개수/[Lv.0] 짝수 홀수 개수_01.py
+++ Programmers/Python/Lv.0/[Lv.0] 짝수 홀수 개수/[Lv.0] 짝수 홀수 개수_01.py
@@ -2,6 +2,7 @@ def solution(num_list):
     answer = [0, 0]
 
     for i in num_list:
-        answer[i % 2] += 1 # 짝수면 짝수, 홀수면 홀수
+        # 짝수면 짝수, 홀수면 홀수
+        answer[i % 2] += 1
 
     return answer
