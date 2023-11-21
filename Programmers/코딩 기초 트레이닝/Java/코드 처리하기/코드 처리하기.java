8857a19ba4a679100ac9f018623da312b256bc1a
ho-ong <honghj8959@gmail.com>
2023-11-21 12:50:31 +0900

    Add Programmers Code (Java)

---
+++ Programmers/코딩 기초 트레이닝/Java/코드 처리하기/코드 처리하기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        int mode = 0;
+
+        for (int i = 0; i < code.length(); i++) {
+            if (code.charAt(i) == '1') {
+                // code의 문자가 '1'일 경우
+                // 0 -> 1, 1 -> 0 변환
+                mode = 1 - mode;
+            } else if (i % 2 == mode) {
+                // mode가 0 또는 1일 경우
+                // code의 문자 추가
+                answer += code.charAt(i);
+            }
+        }
+
+        // 빈 문자열이면 "EMPTY" 반환, 아니면 문자열 반환
+        return "".equals(answer) ? "EMPTY" : answer;
+    }
+}
