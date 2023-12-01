63bd9e47b74ed8ecbf44e4856f6205fec9a8cf47
ho-ong <honghj8959@gmail.com>
2023-12-02 02:02:10 +0900

    Add Programmers Code (Java)

---
+++ Programmers/코딩 기초 트레이닝/Java/길이에 따른 연산/길이에 따른 연산.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int sum = 0;
+        int mul = 1;
+
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list.length >= 11) {
+                sum += num_list[i]; // 합
+                answer = sum;
+            } else {
+                mul *= num_list[i]; // 곱
+                answer = mul;
+            }
+        }
+
+        return answer;
+    }
+}
