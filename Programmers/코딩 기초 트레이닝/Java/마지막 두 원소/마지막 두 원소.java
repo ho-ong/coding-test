6303dca471e03921faa6b498e6f5ec92cb398aca
ho-ong <honghj8959@gmail.com>
2023-12-12 20:16:38 +0900

    Add Programmers Code (Java)

---
+++ Programmers/코딩 기초 트레이닝/Java/마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length + 1];
+        int a = num_list[num_list.length - 1]; // 마지막 원소
+        int b = num_list[num_list.length - 2]; // 그전 원소
+
+        for (int i = 0; i < num_list.length; i++) {
+            answer[i] = num_list[i];
+        }
+
+        if (a > b) {
+            // 마지막 원소가 크면
+            answer[num_list.length] = a - b;
+        } else {
+            // 마지막 원소가 작으면
+            answer[num_list.length] = a * 2;
+        }
+
+        return answer;
+    }
+}
