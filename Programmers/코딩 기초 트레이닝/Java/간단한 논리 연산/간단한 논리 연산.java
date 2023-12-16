1f0408afb330ae70c0c5442299ac8744a480b6db
ho-ong <honghj8959@gmail.com>
2023-12-16 13:53:48 +0900

    Update Programmers Code (Java)

--- Programmers/코딩 기초 트레이닝/Java/간단한 논리 연산/간단한 논리 연산.java
+++ Programmers/코딩 기초 트레이닝/Java/간단한 논리 연산/간단한 논리 연산.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        // 논리 연산자 : &&(and), ||(or), !(not)
         boolean answer = (x1 || x2) && (x3 || x4);
         return answer;
     }
