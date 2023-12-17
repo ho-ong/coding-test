8dd3fa7162fd00fa885b93e0affc3a753691840b
ho-ong <honghj8959@gmail.com>
2023-12-17 16:05:15 +0900

    Update Programmers Code (Java)

--- Programmers/코딩 기초 트레이닝/Java/등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기_01.java
+++ Programmers/코딩 기초 트레이닝/Java/등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기_01.java
@@ -2,7 +2,7 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int a, int d, boolean[] included) {
-        // included의 원소가 true일 경우, 등차수열 더하기
+        // included의 원소가 true일 경우 등차수열 더하기
         return IntStream.range(0, included.length).map(i -> included[i] ? a + (i * d) : 0).sum();
     }
 }
