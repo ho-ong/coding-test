4f03b3a720f61b1458fec6a26bf3d09c5fc939eb
ho-ong <honghj8959@gmail.com>
2023-12-19 03:11:59 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 공 던지기/[Lv.0] 공 던지기_02.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 공 던지기/[Lv.0] 공 던지기_02.java
@@ -1,5 +1,8 @@
 class Solution {
     public int solution(int[] numbers, int k) {
+        // (2 - 1) * 2 % 4 + 1 = 3
+        // (5 - 1) * 2 % 6 + 1 = 3
+        // (3 - 1) * 2 % 3 + 1 = 2
         return (k - 1) * 2 % numbers.length + 1;
     }
 }
