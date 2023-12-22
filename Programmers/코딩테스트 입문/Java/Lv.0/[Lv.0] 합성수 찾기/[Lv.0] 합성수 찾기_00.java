445b3c04f4404fa61dfad536be3d809ae4817353
ho-ong <honghj8959@gmail.com>
2023-12-22 19:45:06 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 합성수 찾기/[Lv.0] 합성수 찾기_00.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 합성수 찾기/[Lv.0] 합성수 찾기_00.java
@@ -3,14 +3,18 @@ class Solution {
         int answer = 0;
 
         for (int i = 1; i <= n; i++) {
-            int count = 0;
+            int count = 0; // count 초기화
 
             for (int j = 1; j <= i; j++) {
+                // 나머지가 0일 경우
+                // count 1씩 증가
                 if (i % j == 0) {
                     count += 1;
                 }
             }
 
+            // count가 3보다 크거나 같을 경우
+            // count 1씩 증가
             if (count >= 3) {
                 answer += 1;
             }
