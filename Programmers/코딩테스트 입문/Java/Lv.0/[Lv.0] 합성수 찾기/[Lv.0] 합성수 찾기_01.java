00844101273972799ca640a4b0ef9392a9261fc7
ho-ong <honghj8959@gmail.com>
2023-12-25 12:44:53 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 합성수 찾기/[Lv.0] 합성수 찾기_01.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 합성수 찾기/[Lv.0] 합성수 찾기_01.java
@@ -2,6 +2,6 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int n) {
-        return (int) IntStream.rangeClosed(1, n).filter(i -> (int) IntStream.rangeClosed(1, i).filter(i2 -> i % i2 == 0).count() > 2).count();
+        return (int) IntStream.rangeClosed(1, n).filter(i1 -> (int) IntStream.rangeClosed(1, i1).filter(i2 -> i1 % i2 == 0).count() > 2).count();
     }
 }
