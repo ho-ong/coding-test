f5372489c74b452de93360ebedc7e7ffee669e99
ho-ong <honghj8959@gmail.com>
2024-01-04 02:37:43 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 세균 증식/[Lv.0] 세균 증식_00.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 세균 증식/[Lv.0] 세균 증식_00.java
@@ -2,7 +2,8 @@ class Solution {
     public int solution(int n, int t) {
         int answer = n;
 
-        for (int i = 1; i <= t; i++) {
+        for (int i = 0; i < t; i++) {
+            // 2를 곱하기 (n, n * 2, n * 2 * 2, ...)
             answer *= 2;
         }
 
--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 세균 증식/[Lv.0] 세균 증식_01.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 세균 증식/[Lv.0] 세균 증식_01.java
@@ -1,6 +1,8 @@
 class Solution {
     public int solution(int n, int t) {
         int answer = 0;
+
+        // 왼쪽으로 1비트 옯긴 값 = 2로 곱한 값
         answer = n << t;
         return answer;
     }
