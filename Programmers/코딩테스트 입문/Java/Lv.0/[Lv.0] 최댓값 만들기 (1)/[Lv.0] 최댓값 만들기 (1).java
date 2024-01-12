d26218448ed306aa9d78a0e0564043a0f1b8c31d
ho-ong <honghj8959@gmail.com>
2024-01-12 21:34:03 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 최댓값 만들기 (1)/[Lv.0] 최댓값 만들기 (1).java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 최댓값 만들기 (1)/[Lv.0] 최댓값 만들기 (1).java
@@ -3,8 +3,6 @@ import java.util.*;
 class Solution {
     public int solution(int[] numbers) {
         Arrays.sort(numbers);
-
-        // 배열의 마지막 원소, 그전 요소를 곱하기 (큰 값끼리 곱하기)
         return numbers[numbers.length - 1] * numbers[numbers.length - 2];
     }
 }
