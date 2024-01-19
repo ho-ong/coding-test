d744854b901cf9a92360baed4589efde53a87a69
ho-ong <honghj8959@gmail.com>
2024-01-20 00:47:11 +0900

    Update Programmers Code (Java)

--- Programmers/코딩 기초 트레이닝/Java/문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
+++ Programmers/코딩 기초 트레이닝/Java/문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -1,6 +1,6 @@
 class Solution {
     public String solution(String my_string, int n) {
-        // substring(startIndex, endIndex) : startIndex(포함)부터 endIndex(불포함)까지의 문자열 반환
+        // substring(index) : index(포함)부터 모든 문자열 반환
         return my_string.substring(my_string.length() - n);
     }
 }
