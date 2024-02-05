831b8b309c2c345e3b75ef626a256c85d7588239
ho-ong <honghj8959@gmail.com>
2024-02-05 09:40:12 +0900

    Update Programmers Code (Java)

--- Programmers/코딩 기초 트레이닝/Java/수 조작하기 1/수 조작하기 1_02.java
+++ Programmers/코딩 기초 트레이닝/Java/수 조작하기 1/수 조작하기 1_02.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int n, String control) {
-        // Stream.reduce(): Stream의 요소들을 하나의 데이터로 변환
+        // Stream.reduce(): Stream의 원소들을 하나의 데이터로 변환
         // Stream.reduce(accumulator), Stream.reduce(init, accumulator)
         return control.chars().reduce(n, (answer, c) -> answer + (c == 'w' ? 1 : c == 's' ? -1 : c == 'd' ? 10 : -10));
     }
--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 최댓값 만들기 (1)/[Lv.0] 최댓값 만들기 (1).java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 최댓값 만들기 (1)/[Lv.0] 최댓값 만들기 (1).java
@@ -4,7 +4,7 @@ class Solution {
     public int solution(int[] numbers) {
         Arrays.sort(numbers);
 
-        // 배열의 마지막 원소, 그전 요소를 곱하기 (큰 값끼리 곱하기)
+        // 배열의 마지막 원소, 그전 원소를 곱하기 (큰 값끼리 곱하기)
         return numbers[numbers.length - 1] * numbers[numbers.length - 2];
     }
 }
