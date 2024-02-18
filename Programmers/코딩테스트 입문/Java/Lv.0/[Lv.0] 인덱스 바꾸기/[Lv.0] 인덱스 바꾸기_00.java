ef7f9618f38aefc62dc3030e8e51e83bbb00386c
ho-ong <honghj8959@gmail.com>
2024-02-19 05:20:11 +0900

    Add Programmers Code (Java)

---
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 인덱스 바꾸기/[Lv.0] 인덱스 바꾸기_00.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String[] str = my_string.split("");
+        String answer = str[num1];
+        str[num1] = str[num2];
+        str[num2] = answer;
+        return String.join("", str);
+    }
+}
---
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 인덱스 바꾸기/[Lv.0] 인덱스 바꾸기_01.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        List<String> list = Arrays.stream(my_string.split("")).collect(Collectors.toList());
+        Collections.swap(list, num1, num2);
+        return String.join("", list);
+    }
+}
