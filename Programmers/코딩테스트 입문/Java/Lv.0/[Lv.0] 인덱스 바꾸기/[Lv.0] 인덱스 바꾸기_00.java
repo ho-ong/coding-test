75652c48c61e88a93d1fb4374e97bd710ab42bc6
ho-ong <honghj8959@gmail.com>
2024-02-20 01:30:27 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 인덱스 바꾸기/[Lv.0] 인덱스 바꾸기_00.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 인덱스 바꾸기/[Lv.0] 인덱스 바꾸기_00.java
@@ -2,8 +2,8 @@ class Solution {
     public String solution(String my_string, int num1, int num2) {
         String[] str = my_string.split("");
         String answer = str[num1];
-        str[num1] = str[num2];
-        str[num2] = answer;
+        str[num1] = str[num2]; // 인덱스 num2로 변경
+        str[num2] = answer; // 인덱스 num1로 변경
         return String.join("", str);
     }
 }
--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 인덱스 바꾸기/[Lv.0] 인덱스 바꾸기_01.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 인덱스 바꾸기/[Lv.0] 인덱스 바꾸기_01.java
@@ -4,6 +4,8 @@ import java.util.stream.Collectors;
 class Solution {
     public String solution(String my_string, int num1, int num2) {
         List<String> list = Arrays.stream(my_string.split("")).collect(Collectors.toList());
+
+        // Collections.swap(list, num1, num2): 목록 내 해당 인덱스에 위치한 두 원소를 교환
         Collections.swap(list, num1, num2);
         return String.join("", list);
     }
