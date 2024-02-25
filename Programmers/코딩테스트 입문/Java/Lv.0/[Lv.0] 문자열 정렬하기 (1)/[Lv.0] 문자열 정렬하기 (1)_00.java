fdcb6c0f9404cb50db1edebedf84bf420a85d72f
ho-ong <honghj8959@gmail.com>
2024-02-26 01:17:35 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 문자열 정렬하기 (1)/[Lv.0] 문자열 정렬하기 (1)_00.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 문자열 정렬하기 (1)/[Lv.0] 문자열 정렬하기 (1)_00.java
@@ -3,16 +3,22 @@ import java.util.*;
 class Solution {
     public int[] solution(String my_string) {
         // [a-z]: a부터 z까지
+        // my_string의 문자열 중 [a-z]를 ""으로 치환
+        // "hi12392" -> "12392"
         String s = my_string.replaceAll("[a-z]", "");
+
+        // "" 기준으로 문자열을 분리하여 str 배열에 저장 후 반환
+        // "12392" -> ["1", "2", "3", "9", "2"]
         String[] str = s.split("");
         int[] answer = new int[str.length];
 
         for (int i = 0; i < str.length; i++) {
-            // 배열에 원소를 넣기
+            // str 배열의 원소를 int형으로 변환 후 넣기
+            // ["1", "2", "3", "9", "2"] -> [1, 2, 3, 9, 2]
             answer[i] = Integer.parseInt(str[i]);
         }
 
-        Arrays.sort(answer);
+        Arrays.sort(answer); // 오름차순 정렬 [1, 2, 2, 3, 9]
         return answer;
     }
 }
--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 문자열 정렬하기 (2)/[Lv.0] 문자열 정렬하기 (2)_00.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 문자열 정렬하기 (2)/[Lv.0] 문자열 정렬하기 (2)_00.java
@@ -3,11 +3,16 @@ import java.util.*;
 class Solution {
     public String solution(String my_string) {
         String answer = "";
+
+        // my_string 문자열 소문자 변환,
+        // "" 기준으로 문자열을 분리하여 str 배열에 저장 후 반환
+        // "Bcad" -> ["b", "c", "a", "d"]
         String[] str = my_string.toLowerCase().split("");
-        Arrays.sort(str);
+        Arrays.sort(str); // 오름차순 정렬 ["a", "b", "c", "d"]
 
         for (int i = 0; i < str.length; i++) {
-            // 배열의 원소를 모두 합치기
+            // str 배열의 원소를 합치기
+            // ["a", "b", "c", "d"] -> "abcd"
             answer += str[i];
         }
 
