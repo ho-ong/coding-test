e9c9a2ce654ca9fa079a0fff488231d82fbf5291
ho-ong <honghj8959@gmail.com>
2024-02-29 00:17:10 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 짝수 홀수 개수/[Lv.0] 짝수 홀수 개수_00.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 짝수 홀수 개수/[Lv.0] 짝수 홀수 개수_00.java
@@ -3,10 +3,14 @@ class Solution {
         int[] answer = new int[2];
 
         for (int i = 0; i < num_list.length; i++) {
+            // num_list 배열의 원소를 2로 나누기
+            // 나머지가 0일 경우 짝수, 1일 경우 홀수
             if (num_list[i] % 2 == 0) {
-                answer[0]++; // 짝수
+                // 짝수, 첫 번째 원소 증가 [1, 0]
+                answer[0]++;
             } else {
-                answer[1]++; // 홀수
+                // 홀수, 두 번째 원소 증가 [0, 1]
+                answer[1]++;
             }
         }
 
