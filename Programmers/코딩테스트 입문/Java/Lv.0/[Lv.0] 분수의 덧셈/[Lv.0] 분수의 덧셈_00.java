18c9cfc80c2aee8bd3ff11a50027dfb2df6850af
ho-ong <honghj8959@gmail.com>
2024-03-05 01:20:03 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 분수의 덧셈/[Lv.0] 분수의 덧셈_00.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 분수의 덧셈/[Lv.0] 분수의 덧셈_00.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int[] answer = new int[2];
-        int max = 1; // 최대공약수 초기화
+        int max = 1; // 최대공약수
 
         // 분수 계산 시 통분, 약분은 필수이다.
         // 약분을 위해 최대공약수를 구한다.
