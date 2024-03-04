274787e264f0f2ca90924f9e7983f2eb45892025
ho-ong <honghj8959@gmail.com>
2024-03-04 13:56:37 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 중앙값 구하기/[Lv.0] 중앙값 구하기_01.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 중앙값 구하기/[Lv.0] 중앙값 구하기_01.java
@@ -2,10 +2,9 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] array) {
-        Arrays.sort(array); // 오름차순 정렬
+        Arrays.sort(array); // 오름차순 정렬 [1, 2, 7, 10, 11]
 
-        // 중앙값 = 배열의 길이 / 2
-        // array 배열의 길이 / 2 = 5 / 2 = 2
+        // 중앙값 = 배열의 길이 / 2 (5 / 2 = 2)
         // array 배열 내 인덱스 2에 위치한 원소 = 7
 
         // 오른쪽으로 1비트 옮긴 값 = 2로 나눈 값
--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 짝수는 싫어요/[Lv.0] 짝수는 싫어요_00.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 짝수는 싫어요/[Lv.0] 짝수는 싫어요_00.java
@@ -1,8 +1,11 @@
 class Solution {
     public int[] solution(int n) {
+        // + 1을 하는 이유?
+        // 입출력 예의 n이 15일 때, result의 배열 길이는 8이다.
+        // 15 / 2 = 7이므로 8이 나오려면 n에 + 1을 해야 한다.
         int[] answer = new int[(n + 1) / 2];
 
-        // 배열의 홀수 원소 구하기 (1부터 시작, 2씩 증가)
+        // 홀수는 1부터 시작, 2씩 증가 (1, 3, 5, ...)
         for (int i = 1; i <= n; i += 2) {
             // i가 1일 경우 answer[0] = 1
             answer[i / 2] = i;
