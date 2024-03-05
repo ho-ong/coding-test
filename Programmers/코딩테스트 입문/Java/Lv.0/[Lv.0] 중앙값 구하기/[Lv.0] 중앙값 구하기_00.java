77bccac7ea128c6bd6023068a4fd0c6cc809c7a3
ho-ong <honghj8959@gmail.com>
2024-03-06 00:52:44 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 중앙값 구하기/[Lv.0] 중앙값 구하기_00.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 중앙값 구하기/[Lv.0] 중앙값 구하기_00.java
@@ -5,7 +5,8 @@ class Solution {
         // array 배열 오름차순 정렬 [1, 2, 7, 10, 11]
         Arrays.sort(array);
 
-        // 중앙값 = 배열의 길이 / 2 (5 / 2 = 2)
+        // 중앙값 = 배열의 길이 / 2
+        // array 배열의 길이 / 2 = 2
         // array 배열 내 인덱스 2에 위치한 원소 = 7
         return array[array.length / 2];
     }
