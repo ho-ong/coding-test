70a0d4caa4d9a1005aa2530e4a94f68075f80f77
ho-ong <honghj8959@gmail.com>
2024-03-07 01:08:59 +0900

    Update Programmers Code (Java)

--- Programmers/코딩 기초 트레이닝/Java/문자열 돌리기/문자열 돌리기_01.java
+++ Programmers/코딩 기초 트레이닝/Java/문자열 돌리기/문자열 돌리기_01.java
@@ -6,7 +6,8 @@ public class Solution {
         String a = sc.next();
 
         // toCharArray(): 문자열을 char형 문자로 분리하고, char[] 배열에 순서대로 저장
-        // 문자열 a를 char형 문자로 분리하고, char[] 배열에 순서대로 저장 -> 문자 c로 변환 ("abcde" -> ['a', 'b', 'c', 'd', 'e'] -> 'a')
+        // 문자열 a를 char형 문자로 분리하고, char[] 배열에 순서대로 저장 -> 문자 c로 변환
+        // "abcde" -> ['a', 'b', 'c', 'd', 'e'] -> 'a'
         for (char c : a.toCharArray()) {
             // 문자 c를 출력하기 (줄 바꿈)
             System.out.println(c);
