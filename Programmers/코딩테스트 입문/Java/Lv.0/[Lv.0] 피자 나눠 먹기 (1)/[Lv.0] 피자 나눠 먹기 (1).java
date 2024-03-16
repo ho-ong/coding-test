48f36ed84f86d23305df6b4b41cf07f420130b0a
ho-ong <honghj8959@gmail.com>
2024-03-17 01:52:13 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 피자 나눠 먹기 (1)/[Lv.0] 피자 나눠 먹기 (1).java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 피자 나눠 먹기 (1)/[Lv.0] 피자 나눠 먹기 (1).java
@@ -2,6 +2,7 @@ class Solution {
     public int solution(int n) {
         // n을 7로 나눴을 때, (피자 1판 7조각)
         // 나머지가 0일 경우 n / 7, 아닐 경우 n / 7 + 1 (최소 피자 1판 필요)
+        // 1 / 7 + 1 = 1
         return n % 7 == 0 ? n / 7 : n / 7 + 1;
     }
 }
