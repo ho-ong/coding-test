d79a6c2504e690be74be4f5bebaa32c532b9c7d9
ho-ong <honghj8959@gmail.com>
2024-03-20 15:07:51 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 아이스 아메리카노/[Lv.0] 아이스 아메리카노_00.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 아이스 아메리카노/[Lv.0] 아이스 아메리카노_00.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] solution(int money) {
-        // 아메리카노 한 잔 = 5,500원
+        // 아메리카노 한 잔 = 5500
         // 아메리카노 잔 수 = 몫, 잔돈 = 나머지
         // return new int[] {money / 5500, money % 5500};
-        int[] answer = {money / 5500, money % 5500};
-        return answer;
+        int[] answer = {money / 5500, money % 5500}; // 1, 0
+        return answer; // [1, 0]
     }
 }
