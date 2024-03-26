8aac6f032142dada13c54e2a52162d438c3610b7
ho-ong <honghj8959@gmail.com>
2024-03-27 06:10:05 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 편지/[Lv.0] 편지.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 편지/[Lv.0] 편지.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(String message) {
-        // 문자열의 길이에 2를 곱하기
+        // 문자열 message의 길이에 2를 곱하기
+        // "happy birthday!" -> 15 * 2 -> 30
         return message.length() * 2;
     }
 }
