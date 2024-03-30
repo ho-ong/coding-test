540c79d506a178517cc1b3cc32b13b784c5da02b
ho-ong <honghj8959@gmail.com>
2024-03-30 09:46:23 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 머쓱이보다 키 큰 사람/[Lv.0] 머쓱이보다 키 큰 사람_00.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 머쓱이보다 키 큰 사람/[Lv.0] 머쓱이보다 키 큰 사람_00.java
@@ -3,9 +3,11 @@ class Solution {
         int answer = 0;
 
         for (int i = 0; i < array.length; i++) {
-            // array 배열의 원소가 height보다 클 경우
+            // array 배열의 원소가 클 경우
+            // [149, 180, 192, 170] > 167
             if (array[i] > height) {
-                answer++; // 증가
+                // answer 증가 -> 3
+                answer++;
             }
         }
 
--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 머쓱이보다 키 큰 사람/[Lv.0] 머쓱이보다 키 큰 사람_01.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 머쓱이보다 키 큰 사람/[Lv.0] 머쓱이보다 키 큰 사람_01.java
@@ -2,7 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] array, int height) {
-        // array 배열의 원소가 height보다 클 경우의 개수 구하기
-        return (int) Arrays.stream(array).filter(i -> i > height).count();
+        // 배열 스트림 생성 -> filter -> 개수 계산
+        // filter(): 스트림 내 원소들을 하나씩 평가하여 추출
+        // count(): 개수 반환
+
+        // i가 클 경우 개수를 추출
+        // [149, 180, 192, 170] > 167
+        return (int) Arrays.stream(array).filter(i -> i > height).count(); // 3
     }
 }
