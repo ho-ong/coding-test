357c24a6c2d6d9f82e95cb515cb21177958f94e2
ho-ong <honghj8959@gmail.com>
2024-03-31 11:06:03 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 모음 제거/[Lv.0] 모음 제거.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 모음 제거/[Lv.0] 모음 제거.java
@@ -4,6 +4,5 @@ class Solution {
         // 정규 표현식(Regular Expression)
         // [aeiou]: a, e, i, o, u (or 연산자)
         return my_string.replaceAll("[aeiou]", "");
-        // return my_string.replaceAll("a|e|i|o|u", "");
     }
 }
