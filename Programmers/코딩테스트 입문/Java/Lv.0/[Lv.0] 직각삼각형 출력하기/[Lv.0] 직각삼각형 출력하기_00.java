6e960f5d861e15f4fed415ce46dd21063dc202ee
ho-ong <honghj8959@gmail.com>
2024-04-01 16:11:47 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 직각삼각형 출력하기/[Lv.0] 직각삼각형 출력하기_00.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 직각삼각형 출력하기/[Lv.0] 직각삼각형 출력하기_00.java
@@ -7,7 +7,9 @@ public class Solution {
 
         // 높이와 너비가 n인 직각 이등변 삼각형
         for (int i = 1; i <= n; i++) {
-            // *을 i번 반복하기
+            // repeat(): 문자열 반복
+            // *을 i번 반복
+            // println(): 출력 (줄 바꿈 O)
             System.out.println("*".repeat(i));
         }
     }
--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 직각삼각형 출력하기/[Lv.0] 직각삼각형 출력하기_01.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 직각삼각형 출력하기/[Lv.0] 직각삼각형 출력하기_01.java
@@ -8,10 +8,11 @@ public class Solution {
         // 높이와 너비가 n인 직각 이등변 삼각형
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
-                // *을 j번 반복하기
+                // *을 j번 반복
                 System.out.print("*");
             }
 
+            // println(): 출력 (줄 바꿈 O)
             System.out.println();
         }
     }
