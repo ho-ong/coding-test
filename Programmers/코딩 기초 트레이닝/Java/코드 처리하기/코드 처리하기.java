d7d4d815ccecf6169664950168604680f7823841
ho-ong <honghj8959@gmail.com>
2024-04-05 14:52:45 +0900

    Update Programmers Code (Java)

--- Programmers/코딩 기초 트레이닝/Java/코드 처리하기/코드 처리하기.java
+++ Programmers/코딩 기초 트레이닝/Java/코드 처리하기/코드 처리하기.java
@@ -9,7 +9,7 @@ class Solution {
 
             // 문자열 code의 문자가 '1'일 경우
             if (code.charAt(i) == '1') {
-                // mode(0 또는 1)를 바꾸기 (0 -> 1, 1 -> 0)
+                // mode(0 또는 1)를 바꾸기 (0 <-> 1)
                 mode = 1 - mode; // 1 - 0 = 1
             // i를 2로 나눴을 때, 나머지가 mode(0 또는 1)일 경우
             } else if (i % 2 == mode) {
