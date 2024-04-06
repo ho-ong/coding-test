79832a6f99be739fe36c7f9312d618bcb1df26e1
ho-ong <honghj8959@gmail.com>
2024-04-06 18:57:54 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 순서쌍의 개수/[Lv.0] 순서쌍의 개수_00.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 순서쌍의 개수/[Lv.0] 순서쌍의 개수_00.java
@@ -2,11 +2,13 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        // n의 순서쌍 = n의 약수
         for (int i = 1; i <= n; i++) {
-            // 나머지가 0일 경우 n의 약수
+            // n의 순서쌍 = n의 약수
+            // n을 i로 나눴을 때, 나머지가 0일 경우 n의 약수
+            // 1, 2, 4, 5, 10, 20
             if (n % i == 0) {
-                answer++; // 증가
+                // answer 증가
+                answer++; // 6
             }
         }
 
--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 순서쌍의 개수/[Lv.0] 순서쌍의 개수_01.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 순서쌍의 개수/[Lv.0] 순서쌍의 개수_01.java
@@ -2,8 +2,17 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int n) {
+        // 기본형 스트림 생성 -> filter -> 개수 계산
+        // 기본형 스트림: IntStream, LongStream, DoubleStream (숫자 스트림을 효율적으로 처리)
+        // rangeClosed(start, end): start(포함)부터 end(포함)까지 범위 내 정수를 순서대로 생성
+        // filter(): 스트림 내 원소 중 특정 조건에 해당하는 원소의 값만 추출하여 반환
+        // count(): 개수 반환
+
+        // 1(포함)부터 n(포함)까지 범위 내 정수를 순서대로 생성 (1, 2, 3, ..., 18, 19, 20)
         // n의 순서쌍 = n의 약수
-        // 나머지가 0일 경우 n의 약수, n의 약수의 개수 구하기
-        return (int) IntStream.rangeClosed(1, n).filter(i -> n % i == 0).count();
+        // n을 i로 나눴을 때, 나머지가 0일 경우 n의 약수
+        // n의 약수의 개수를 구하기
+        // 1, 2, 4, 5, 10, 20
+        return (int) IntStream.rangeClosed(1, n).filter(i -> n % i == 0).count(); // 6
     }
 }
