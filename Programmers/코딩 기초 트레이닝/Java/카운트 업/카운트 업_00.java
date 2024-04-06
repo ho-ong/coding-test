64039426a0216f36244f67805ff7cd0b6644ef5e
ho-ong <honghj8959@gmail.com>
2024-04-06 18:06:17 +0900

    Update Programmers Code (Java)

--- Programmers/코딩 기초 트레이닝/Java/카운트 업/카운트 업_00.java
+++ Programmers/코딩 기초 트레이닝/Java/카운트 업/카운트 업_00.java
@@ -1,9 +1,14 @@
 class Solution {
     public int[] solution(int start_num, int end_num) {
+        // + 1 하는 이유?
+        // 입출력 예의 start_num이 3, end_num이 10일 때, result의 배열 길이는 8이다.
+        // 10 - 3 = 7이므로 8이 나오려면 + 1을 해야 한다.
         int[] answer = new int[end_num - start_num + 1];
 
         for (int i = 0; i < answer.length; i++) {
             // start_num 증가
+            // i = 0 -> answer[0] = 3, ..., answer[7] = 10
+            // 3, 4, 5, 6, 7, 8, 9, 10 -> [3, 4, 5, 6, 7, 8, 9, 10]
             answer[i] = start_num++;
         }
 
--- Programmers/코딩 기초 트레이닝/Java/카운트 업/카운트 업_01.java
+++ Programmers/코딩 기초 트레이닝/Java/카운트 업/카운트 업_01.java
@@ -2,7 +2,12 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        // IntStream.rangeClosed(start, end): 범위 내 정수를 순서대로 생성
+        // 기본형 스트림 생성 -> 배열 변환
+        // 기본형 스트림: IntStream, LongStream, DoubleStream (숫자 스트림을 효율적으로 처리)
+        // rangeClosed(start, end): start(포함)부터 end(포함)까지 범위 내 정수를 순서대로 생성
+
+        // start_num(포함)부터 end_num(포함)까지 범위 내 정수를 순서대로 생성 (3, 4, 5, ..., 8, 9, 10)
+        // 3, 4, 5, 6, 7, 8, 9, 10 -> [3, 4, 5, 6, 7, 8, 9, 10]
         return IntStream.rangeClosed(start_num, end_num).toArray();
     }
 }
