57da0db0ffeb1b8c62c82753e0145d3388357224
ho-ong <honghj8959@gmail.com>
2024-04-06 18:05:51 +0900

    Add Programmers Code (Java)

---
+++ Programmers/코딩 기초 트레이닝/Java/수열과 구간 쿼리 4/수열과 구간 쿼리 4_01.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        // [s, e, k] -> [0, 4, 1]
+        // s = queries[j][0] -> 0
+        // e = queries[j][1] -> 4
+        // k = queries[j][2] -> 1
+
+        for (int[] query : queries) {
+            // s ≤ i ≤ e -> 0 ≤ i ≤ 4
+            for (int i = query[0]; i <= query[1]; i++) {
+                // i = k의 배수
+                // i를 k로 나눴을 때, 나머지가 0일 경우 k의 배수
+                if (i % query[2] == 0) {
+                    // arr[i] 증가
+                    arr[i]++;
+                }
+            }
+        }
+
+        // [0, 1, 2, 4, 3] -> [1, 2, 3, 5, 4] -> [2, 2, 4, 5, 4] -> [3, 2, 4, 6, 4]
+        return arr;
+    }
+}
