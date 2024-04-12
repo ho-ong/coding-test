7ad56acbdd76ec9e8acee07834fef853a2ad09ca
ho-ong <honghj8959@gmail.com>
2024-04-12 21:44:02 +0900

    Update Programmers Code (Java)

--- Programmers/코딩 기초 트레이닝/Java/콜라츠 수열 만들기/콜라츠 수열 만들기_00.java
+++ Programmers/코딩 기초 트레이닝/Java/콜라츠 수열 만들기/콜라츠 수열 만들기_00.java
@@ -1,19 +1,28 @@
 class Solution {
     public int[] solution(int n) {
+        // 정수를 문자열로 변환
+        // "10 "
         String s = n + " ";
 
         // n이 1이 아닐 경우
         while (n != 1) {
+            // n을 2로 나눴을 때, 나머지가 0일 경우 짝수, 아닐 경우 홀수
             // 짝수일 경우 n / 2, 홀수일 경우 n * 3 + 1
             n = n % 2 == 0 ? n / 2 : n * 3 + 1;
+
+            // "10 5 16 8 4 2 1 "
             s += n + " ";
         }
 
+        // split(): 문자열 분리
+        // 문자열 s를 " " 기준으로 분리하고, str 배열에 순서대로 저장
+        // "10 5 16 8 4 2 1 " -> ["10", "5", "16", "8", "4", "2", "1"]
         String[] str = s.split(" ");
         int[] answer = new int[str.length];
 
         for (int i = 0; i < answer.length; i++) {
-            // 배열에 원소를 넣기
+            // str 배열의 원소를 정수로 변환하여 넣기
+            // ["10", "5", "16", "8", "4", "2", "1"] -> [10, 5, 16, 8, 4, 2, 1]
             answer[i] = Integer.parseInt(str[i]);
         }
 
--- Programmers/코딩 기초 트레이닝/Java/콜라츠 수열 만들기/콜라츠 수열 만들기_01.java
+++ Programmers/코딩 기초 트레이닝/Java/콜라츠 수열 만들기/콜라츠 수열 만들기_01.java
@@ -2,16 +2,27 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
+        // list 생성
         List<Integer> list = new ArrayList<>();
-        list.add(n); // 추가
+
+        // list에 원소 추가
+        // [10]
+        list.add(n);
 
         // n이 1이 아닐 경우
         while (n != 1) {
+            // n을 2로 나눴을 때, 나머지가 0일 경우 짝수, 아닐 경우 홀수
             // 짝수일 경우 n / 2, 홀수일 경우 n * 3 + 1
             n = n % 2 == 0 ? n / 2 : n * 3 + 1;
-            list.add(n); // 추가
+
+            // list에 원소 추가
+            // [10, 5, 16, 8, 4, 2, 1]
+            list.add(n);
         }
 
-        return list.stream().mapToInt(i -> i).toArray();
+        // 리스트 -> 배열
+        // 리스트 스트림 생성 -> mapToInt -> 배열 변환
+        // mapToInt(): 스트림을 IntStream으로 변환 (Integer -> int)
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
