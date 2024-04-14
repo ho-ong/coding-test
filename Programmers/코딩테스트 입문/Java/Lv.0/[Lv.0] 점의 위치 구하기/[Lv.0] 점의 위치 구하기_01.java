3357e684f7dd98becf9bbf48ea958081e506ab00
ho-ong <honghj8959@gmail.com>
2024-04-14 21:24:56 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 점의 위치 구하기/[Lv.0] 점의 위치 구하기_01.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 점의 위치 구하기/[Lv.0] 점의 위치 구하기_01.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int[] dot) {
-        // 제1사분면, 제2사분면, 제3사분면, 제4사분면
+        // dot[0], dot[1] 모두 양수일 경우 제1사분면, dot[0] 음수, dot[1] 양수일 경우 제2사분면
+        // dot[0], dot[1] 모두 음수일 경우 제3사분면, 아닐 경우 제4사분면
         return dot[0] > 0 && dot[1] > 0 ? 1 : dot[0] < 0 && dot[1] > 0 ? 2 : dot[0] < 0 && dot[1] < 0 ? 3 : 4;
     }
 }
