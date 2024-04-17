c1dae430732ea86af44981cd45b71dc4ec7de5d6
ho-ong <honghj8959@gmail.com>
2024-04-17 18:49:54 +0900

    Update Programmers Code (Java)

--- Programmers/코딩 기초 트레이닝/Java/간단한 논리 연산/README.md
+++ Programmers/코딩 기초 트레이닝/Java/간단한 논리 연산/README.md
@@ -3,7 +3,7 @@
 <br>
 
 ## 문제설명
-boolean 변수 `x1`, `x2`, `x3`, `x4`가 매개변수로 주어질 때, 다음의 식의 true/false를 return 하도록 solution 함수를 완성해 주세요.
+boolean 변수 `x1`, `x2`, `x3`, `x4`가 매개변수로 주어질 때, 다음의 식의 true/false를 return 하는 solution 함수를 작성해 주세요.
 
 - (`x1` ∨ `x2`) ∧ (`x3` ∨ `x4`)
 
--- Programmers/코딩 기초 트레이닝/Java/간단한 논리 연산/간단한 논리 연산.java
+++ Programmers/코딩 기초 트레이닝/Java/간단한 논리 연산/간단한 논리 연산.java
@@ -1,6 +1,10 @@
 class Solution {
     public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-        // 논리 연산자: &&(and), ||(or), !(not)
+        // 논리연산자: &&(and), ||(or), !(not)
+        // ∧ = &&(and), ∨ = ||(or)
+
+        // (x1 ∨ x2) ∧ (x3 ∨ x4) ≡ (F ∨ T) ∧ (T ∨ T) ≡ T ∧ T ≡ T
+        // (false || true) && (true || true) = true && true = true
         boolean answer = (x1 || x2) && (x3 || x4);
         return answer;
     }
