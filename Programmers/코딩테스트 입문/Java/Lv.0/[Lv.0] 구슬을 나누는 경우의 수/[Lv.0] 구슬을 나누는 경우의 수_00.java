0490d18f81020a35fd27826d53ad0c00b240bea1
ho-ong <honghj8959@gmail.com>
2024-04-19 20:28:03 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 구슬을 나누는 경우의 수/[Lv.0] 구슬을 나누는 경우의 수_00.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 구슬을 나누는 경우의 수/[Lv.0] 구슬을 나누는 경우의 수_00.java
@@ -3,11 +3,15 @@ import java.math.BigInteger;
 class Solution {
     // 팩토리얼 함수
     public BigInteger fac(int num) {
-        // BigInteger: 큰 정수를 처리, 문자열 형태로 숫자를 처리
+        // BigInteger: 무한대 정수, 큰 정수 처리 시 사용
+        // 문자열 형태로 정수를 처리하므로 큰 정수를 담는 것이 가능 (초기화 시 매개변수는 문자열)
+        // String.valueOf(): 문자열로 변환
         BigInteger big = new BigInteger("1");
 
-        // 팩토리얼 (1 * 2, 1 * 2 * 3, ...)
+        // 팩토리얼 구하기
         for (int i = 1; i <= num; i++) {
+            // balls (num = 3) -> "1", "2", "3" -> 1 * 2 * 3 = 6
+            // share (num = 2) -> "1", "2" -> 1 * 2 = 2
             big = big.multiply(new BigInteger(String.valueOf(i)));
         }
 
@@ -15,8 +19,12 @@ class Solution {
     }
 
     public BigInteger solution(int balls, int share) {
+        // BigInteger 사칙연산 시 BigIntger 클래스 내부 메서드 사용 (add(), subtract(), multiply(), divide(), remainder())
+        // 문자열이므로 일반적인 사칙연산 불가 (+, -, *, /, % 사용 불가)
+
         // balls = n, share = m
-        // 경우의 수 = n! / (n - m)! * m!
+        // 경우의 수 = n! / ((n - m)! * m!)
+        // 3! / ((3 - 2)! * 2!) = 6 / (1 * 2) = 3
         return fac(balls).divide((fac(balls - share).multiply(fac(share))));
     }
 }
--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 구슬을 나누는 경우의 수/[Lv.0] 구슬을 나누는 경우의 수_01.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 구슬을 나누는 경우의 수/[Lv.0] 구슬을 나누는 경우의 수_01.java
@@ -2,16 +2,26 @@ import java.math.BigInteger;
 
 class Solution {
     // 팩토리얼 함수
-    public BigInteger fac(int n) {
-        // BigInteger: 큰 정수를 처리, 문자열 형태로 숫자를 처리
+    public BigInteger fac(int num) {
+        // BigInteger: 무한대 정수, 큰 정수 처리 시 사용
+        // 문자열 형태로 정수를 처리하므로 큰 정수를 담는 것이 가능 (초기화 시 매개변수는 문자열)
+        // String.valueOf(): 문자열로 변환
         BigInteger answer = new BigInteger("1");
-        BigInteger from = new BigInteger("1");
-        BigInteger to = new BigInteger(String.valueOf(n));
+        BigInteger start = new BigInteger("1"); // 시작 (1부터 시작)
+        BigInteger end = new BigInteger(String.valueOf(num)); // 마지막
+
+        // 팩토리얼 구하기
 
         // compareTo(): 숫자 또는 문자열을 비교하여 값 반환
-        // 숫자의 비교: 크다(1), 같다(0), 작다(-1)
-        // 문자열의 비교: 같다(0), 양수/음수 값
-        for (BigInteger i = from; i.compareTo(to) <= 0; i = i.add(BigInteger.ONE)) {
+        // 숫자 비교 값: 같다(0), 크다(1), 작다(-1)
+        // 문자열 비교 값: 같다(0), 앞순(양수), 뒷순(음수) -> 사전적인 순서
+        // i.compareTo(end) <= 0 -> "1"일 경우 start와 같으므로 0, "2"부터는 "1"의 뒷순이므로 음수
+
+        // BigInteger 변수: ZERO(0), ONE(1), TEN(10)
+        // i = i.add(BigInteger.ONE) -> 1씩 증가
+        for (BigInteger i = start; i.compareTo(end) <= 0; i = i.add(BigInteger.ONE)) {
+            // balls (end = 3) -> 1 * 2 * 3 = 6
+            // share (end = 2) -> 1 * 2 = 2
             answer = answer.multiply(i);
         }
 
@@ -19,8 +29,12 @@ class Solution {
     }
 
     public BigInteger solution(int balls, int share) {
+        // BigInteger 사칙연산 시 BigIntger 클래스 내부 메서드 사용 (add(), subtract(), multiply(), divide(), remainder())
+        // 문자열이므로 일반적인 사칙연산 불가 (+, -, *, /, % 사용 불가)
+
         // balls = n, share = m
-        // 경우의 수 = n! / (n - m)! * m!
+        // 경우의 수 = n! / ((n - m)! * m!)
+        // 3! / ((3 - 2)! * 2!) = 6 / (1 * 2) = 3
         return fac(balls).divide((fac(balls - share).multiply(fac(share))));
     }
 }
