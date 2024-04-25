70727ba8b3468a099945b10cc4604e0fc32f9b00
ho-ong <honghj8959@gmail.com>
2024-04-25 13:15:59 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 369게임/[Lv.0] 369게임_01.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 369게임/[Lv.0] 369게임_01.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public int solution(int order) {
-        // 나머지가 3, 6, 9일 경우의 개수
+        // order를 10으로 나눴을 때, 나머지가 3, 6, 9일 경우
         return (int) Arrays.stream(String.valueOf(order).split("")).map(Integer::parseInt).filter(i -> i == 3 || i == 6 || i == 9).count();
     }
 }
