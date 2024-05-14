3005bc9f4dd289ffea4ee13a80cc245657830160
ho-ong <honghj8959@gmail.com>
2024-05-14 19:06:57 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 문자열 정렬하기 (2)/[Lv.0] 문자열 정렬하기 (2)_00.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 문자열 정렬하기 (2)/[Lv.0] 문자열 정렬하기 (2)_00.java
@@ -6,13 +6,11 @@ class Solution {
 
         // my_string 문자열 소문자 변환,
         // "" 기준으로 문자열을 분리하여 str 배열에 저장 후 반환
-        // "Bcad" -> ["b", "c", "a", "d"]
         String[] str = my_string.toLowerCase().split("");
-        Arrays.sort(str); // 오름차순 정렬 ["a", "b", "c", "d"]
+        Arrays.sort(str); // 오름차순 정렬
 
         for (int i = 0; i < str.length; i++) {
             // str 배열의 원소를 합치기
-            // ["a", "b", "c", "d"] -> "abcd"
             answer += str[i];
         }
 
