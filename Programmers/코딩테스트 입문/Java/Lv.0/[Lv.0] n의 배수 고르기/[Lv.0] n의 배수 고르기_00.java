e3b23b1f0d2d8b63079b57f96b54e4ed2f16988a
ho-ong <honghj8959@gmail.com>
2024-05-15 11:53:06 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] n의 배수 고르기/[Lv.0] n의 배수 고르기_00.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] n의 배수 고르기/[Lv.0] n의 배수 고르기_00.java
@@ -7,14 +7,16 @@ class Solution {
 
         // numlist 배열의 원소를 num에 넣기
         for (int num : numlist) {
-            // 나머지가 0일 경우 n의 배수
+            // num을 n으로 나눴을 때, 나머지가 0일 경우 n의 배수
             if (num % n == 0) {
-                list.add(num); // list에 추가
+                // list에 추가
+                list.add(num);
             }
         }
 
-        // list의 원소를 int형으로 변환하여 배열 생성하기
-        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
-        return answer;
+        // 리스트 -> 배열
+        // 리스트 스트림 생성 -> mapToInt -> 배열 변환
+        // mapToInt(): 스트림을 IntStream으로 변환 (Integer -> int)
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] n의 배수 고르기/[Lv.0] n의 배수 고르기_01.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] n의 배수 고르기/[Lv.0] n의 배수 고르기_01.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, int[] numlist) {
-        // 나머지가 0일 경우 n의 배수
+        // i를 n으로 나눴을 때, 나머지가 0일 경우 n의 배수
         return Arrays.stream(numList).filter(i -> i % n == 0).toArray();
     }
 }
