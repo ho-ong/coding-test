f768a59c8ce0101c9fd9a0178df9ede52ee4584d
ho-ong <honghj8959@gmail.com>
2024-05-19 02:45:42 +0900

    Update Programmers Code (Java)

--- Programmers/코딩 기초 트레이닝/Java/정리완료/n의 배수/n의 배수.java
+++ Programmers/코딩 기초 트레이닝/Java/정리완료/n의 배수/n의 배수.java
@@ -1,5 +1,6 @@
 class Solution {
     public int solution(int num, int n) {
+        // 배수: 어떤 수에 정수를 곱한 수 (어떤 수에 의해 나누어 떨어지는 수)
         // num을 n으로 나눴을 때, 나머지가 0일 경우 배수
         // 배수일 경우 1, 아닐 경우 0
         return num % n == 0 ? 1 : 0;
