e437d3a0edf5f3bf3816e366767dd4f874c7f9ff
ho-ong <honghj8959@gmail.com>
2024-05-20 07:38:40 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/정리중/[Lv.0] 머쓱이보다 키 큰 사람/[Lv.0] 머쓱이보다 키 큰 사람_00.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/정리중/[Lv.0] 머쓱이보다 키 큰 사람/[Lv.0] 머쓱이보다 키 큰 사람_00.java
@@ -5,8 +5,7 @@ class Solution {
         for (int i = 0; i < array.length; i++) {
             // array 배열의 원소가 클 경우
             if (array[i] > height) {
-                // answer 증가
-                answer++;
+                answer++; // answer 증가
             }
         }
 
--- Programmers/코딩테스트 입문/Java/Lv.0/정리중/[Lv.0] 머쓱이보다 키 큰 사람/[Lv.0] 머쓱이보다 키 큰 사람_01.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/정리중/[Lv.0] 머쓱이보다 키 큰 사람/[Lv.0] 머쓱이보다 키 큰 사람_01.java
@@ -6,7 +6,7 @@ class Solution {
         // filter(): 스트림 내 원소 중 특정 조건에 해당하는 원소의 값만 추출하여 반환
         // count(): 개수 반환
 
-        // i가 클 경우 개수를 구하기
+        // i가 클 경우 개수 반환
         return (int) Arrays.stream(array).filter(i -> i > height).count();
     }
 }
