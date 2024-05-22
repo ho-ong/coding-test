917dc0cc9c6818d0a13475c7b230270e3db189f8
ho-ong <honghj8959@gmail.com>
2024-05-22 14:14:51 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/정리완료/[Lv.0] 모음 제거/[Lv.0] 모음 제거.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/정리완료/[Lv.0] 모음 제거/[Lv.0] 모음 제거.java
@@ -1,7 +1,6 @@
 class Solution {
     public String solution(String my_string) {
         // replaceAll(regex, replacement): 문자열을 다른 문자열로 치환 (regex: 정규표현식)
-        // a, e, i, o, u를 ""으로 치환 => ""으로 치환할 경우 문자열 제거
 
         // 방법 1
         // [aeiou]: a, e, i, o, u (or)
