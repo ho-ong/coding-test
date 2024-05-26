2695ef678d90609ee99d62c2aea61aefb6960b8c
ho-ong <honghj8959@gmail.com>
2024-05-27 07:56:05 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/정리완료/[Lv.0] 나이 출력/[Lv.0] 나이 출력.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/정리완료/[Lv.0] 나이 출력/[Lv.0] 나이 출력.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int age) {
         // + 1 하는 이유?
-        // 나이는 태어난 연도에 1살 (1부터 시작)
+        // 나이는 태어난 연도에 1살이다. (1부터 시작)
         return 2022 - age + 1;
     }
 }
--- Programmers/코딩테스트 입문/Java/Lv.0/정리완료/[Lv.0] 모음 제거/[Lv.0] 모음 제거_00.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/정리완료/[Lv.0] 모음 제거/[Lv.0] 모음 제거_00.java
@@ -1,7 +1,6 @@
 class Solution {
     public String solution(String my_string) {
         // replaceAll(regex, replacement): 문자열을 다른 문자열로 치환 (regex: 정규표현식)
-
         // [aeiou]: a, e, i, o, u (or)
         return my_string.replaceAll("[aeiou]", "");
     }
