1d2ea661758d3d9d357c9de2be983a19810ced9a
ho-ong <honghj8959@gmail.com>
2024-05-29 12:10:42 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/정리완료/[Lv.0] 짝수 홀수 개수/[Lv.0] 짝수 홀수 개수_01.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/정리완료/[Lv.0] 짝수 홀수 개수/[Lv.0] 짝수 홀수 개수_01.java
@@ -3,8 +3,8 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        // 기본형 스트림 생성 -> 원소 추출 -> 개수 계산 -> 배열 변환
-        // of(): 지정한 값의 정수를 순서대로 생성
+        // 기본형 스트림 생성 (배열 스트림 생성 -> 원소 추출 -> 개수 계산) -> 배열 변환
+        // of(): 지정한 정수를 순서대로 생성
         // filter(): 조건에 해당하는 원소를 추출하여 반환
         // count(): 개수 반환
         // toArray(): 배열 변환
