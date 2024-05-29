86e732793b68644f7a33d3dd5533bf8fdc39ccf0
ho-ong <honghj8959@gmail.com>
2024-05-29 12:02:54 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/정리완료/[Lv.0] 개미 군단/[Lv.0] 개미 군단.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/정리완료/[Lv.0] 개미 군단/[Lv.0] 개미 군단.java
@@ -1,8 +1,7 @@
 class Solution {
     public int solution(int hp) {
-        // 최소 병력 구하기 (장군개미 -> 병정개미 -> 일개미)
-        // 몫 먼저 구하고, 나머지 구하기
         // 몫 = 마리
+        // 장군개미 -> 병정개미 -> 일개미
         return (hp / 5) + (hp % 5 / 3) + (hp % 5 % 3);
     }
 }
