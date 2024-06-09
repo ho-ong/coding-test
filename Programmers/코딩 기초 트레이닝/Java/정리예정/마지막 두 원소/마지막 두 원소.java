cc7921f0aeb700324796450fb8a4b1142d078571
ho-ong <honghj8959@gmail.com>
2024-06-10 05:04:24 +0900

    Update Programmers Code (Java)

--- Programmers/코딩 기초 트레이닝/Java/정리예정/마지막 두 원소/마지막 두 원소.java
+++ Programmers/코딩 기초 트레이닝/Java/정리예정/마지막 두 원소/마지막 두 원소.java
@@ -3,20 +3,14 @@ class Solution {
         // + 1 하는 이유?
         // result의 배열 길이가 1만큼 더 길다.
         int[] answer = new int[num_list.length + 1];
-
-        // num_list 배열의 마지막 원소
-        int a = num_list[num_list.length - 1];
-
-        // num_list 배열의 그전 원소
-        int b = num_list[num_list.length - 2];
+        int a = num_list[num_list.length - 1]; // 배열의 마지막 원소
+        int b = num_list[num_list.length - 2]; // 배열의 마지막 이전 원소
 
         for (int i = 0; i < num_list.length; i++) {
-            // num_list 배열의 원소를 넣기
             answer[i] = num_list[i];
         }
 
-        // answer 배열의 마지막 원소
-        // num_list 배열의 마지막 원소가 클 경우 a - b, 아닐 경우 a * 2
+        // 배열의 마지막 원소가 클 경우 a - b, 아닐 경우 a * 2
         if (a > b) {
             answer[num_list.length] = a - b;
         } else {
--- Programmers/코딩 기초 트레이닝/Java/정리예정/이어 붙인 수/이어 붙인 수.java
+++ Programmers/코딩 기초 트레이닝/Java/정리예정/이어 붙인 수/이어 붙인 수.java
@@ -11,17 +11,14 @@ class Solution {
         // Integer.valueOf(): Integer 클래스의 객체 반환 (parseInt()를 호출하는 형태)
 
         for (int i = 0; i < num_list.length; i++) {
-            // num_list 배열의 원소를 2로 나눴을 때, 나머지가 0일 경우
+            // 배열의 원소를 2로 나눴을 때, 나머지가 0일 경우
             if (num_list[i] % 2 == 0) {
-                // num_list 배열의 원소 중 짝수만 합치기
                 even += String.valueOf(num_list[i]);
             } else {
-                // num_list 배열의 원소 중 홀수만 합치기
                 odd += String.valueOf(num_list[i]);
             }
         }
 
-        // 홀수와 짝수를 더하기
         return Integer.parseInt(even) + Integer.parseInt(odd);
     }
 }
