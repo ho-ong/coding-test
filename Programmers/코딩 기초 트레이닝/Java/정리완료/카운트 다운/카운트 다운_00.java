750ff8a839ed556db5afb9c37264b90aa71cf751
ho-ong <honghj8959@gmail.com>
2024-06-12 08:23:56 +0900

    Update Programmers Code (Java)

--- Programmers/코딩 기초 트레이닝/Java/정리완료/카운트 다운/카운트 다운_00.java
+++ Programmers/코딩 기초 트레이닝/Java/정리완료/카운트 다운/카운트 다운_00.java
@@ -1,13 +1,13 @@
 class Solution {
-    public int[] solution(int start, int end_num) {
+    public int[] solution(int start_num, int end_num) {
         // + 1 하는 이유?
         // 입출력 예의 start_num이 10, end_num이 3일 때, result의 배열 길이는 8이다.
         // 10 - 3 = 7이므로 8이 나오려면 + 1을 해야 한다.
-        int[] answer = new int[start - end_num + 1];
+        int[] answer = new int[start_num - end_num + 1];
 
         // 카운트 감소
         for (int i = 0; i < answer.length; i++) {
-            answer[i] = start--;
+            answer[i] = start_num--;
         }
 
         return answer;
