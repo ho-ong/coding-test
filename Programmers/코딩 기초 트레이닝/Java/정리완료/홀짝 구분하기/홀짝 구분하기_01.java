f0d8948fcef901f03c1ec0a6d955a39f8b16ee1a
ho-ong <honghj8959@gmail.com>
2024-06-17 06:19:07 +0900

    Update Programmers Code (Java)

--- Programmers/코딩 기초 트레이닝/Java/정리완료/홀짝 구분하기/홀짝 구분하기_01.java
+++ Programmers/코딩 기초 트레이닝/Java/정리완료/홀짝 구분하기/홀짝 구분하기_01.java
@@ -5,7 +5,8 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // n을 2로 나눴을 때, 나머지가 0일 경우 짝수, 아닐 경우 홀수
+        // n을 2로 나눴을 때,
+        // 나머지가 0일 경우 짝수, 아닐 경우 홀수
         System.out.println(n + " is " + (n % 2 == 0 ? "even" : "odd"));
     }
 }
