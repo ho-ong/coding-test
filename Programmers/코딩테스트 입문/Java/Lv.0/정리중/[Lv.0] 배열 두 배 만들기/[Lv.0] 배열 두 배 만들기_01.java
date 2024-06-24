87cbab8315bc4920daf7bacc790f450ac86ba087
ho-ong <honghj8959@gmail.com>
2024-06-24 16:08:43 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/정리중/[Lv.0] 배열 두 배 만들기/[Lv.0] 배열 두 배 만들기_01.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/정리중/[Lv.0] 배열 두 배 만들기/[Lv.0] 배열 두 배 만들기_01.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] numbers) {
         // 배열 스트림 생성 -> 원소 매핑 -> 배열 변환
-        // map(): 스트림 내 기존 원소를 새로운 원소로 매핑하여 반환
+        // map(): 다른 형태의 원소로 매핑하여 반환
         return Arrays.stream(numbers).map(i -> i * 2).toArray();
     }
 }
