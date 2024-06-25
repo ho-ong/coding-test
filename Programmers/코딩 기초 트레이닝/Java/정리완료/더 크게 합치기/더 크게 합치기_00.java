15ebb483f69d56b73adf629b4b94549094abb0b2
ho-ong <honghj8959@gmail.com>
2024-06-25 12:40:45 +0900

    Update Programmers Code (Java)

--- Programmers/코딩 기초 트레이닝/Java/정리완료/더 크게 합치기/더 크게 합치기_00.java
+++ Programmers/코딩 기초 트레이닝/Java/정리완료/더 크게 합치기/더 크게 합치기_00.java
@@ -1,10 +1,10 @@
 class Solution {
     public int solution(int a, int b) {
+        // Integer.parseInt(): 정수 변환
         // String.valueOf(): 문자열 변환
         String ab = String.valueOf(a) + String.valueOf(b);
         String ba = String.valueOf(b) + String.valueOf(a);
 
-        // Integer.parseInt(): 정수 변환
         // ab가 ba보다 크거나 같을 경우 ab, 아닐 경우 ba
         if (Integer.parseInt(ab) >= Integer.parseInt(ba)) {
             return Integer.parseInt(ab);
