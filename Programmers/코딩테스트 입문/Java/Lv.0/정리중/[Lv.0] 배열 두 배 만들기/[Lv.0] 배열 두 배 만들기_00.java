da8f00d041eeb3c8d6baa2b2527e021ea1fe884b
ho-ong <honghj8959@gmail.com>
2024-06-27 16:23:48 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/정리중/[Lv.0] 배열 두 배 만들기/[Lv.0] 배열 두 배 만들기_00.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/정리중/[Lv.0] 배열 두 배 만들기/[Lv.0] 배열 두 배 만들기_00.java
@@ -2,6 +2,7 @@ class Solution {
     public int[] solution(int[] numbers) {
         int[] answer = new int[numbers.length];
 
+        // 두 배 증가
         for (int i = 0; i < numbers.length; i++) {
             answer[i] = numbers[i] * 2;
         }
