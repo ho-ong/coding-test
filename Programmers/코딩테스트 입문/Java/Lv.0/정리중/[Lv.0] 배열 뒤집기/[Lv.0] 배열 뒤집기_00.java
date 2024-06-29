7f38205f253da2c015739650364c2a608d9688a4
ho-ong <honghj8959@gmail.com>
2024-06-29 16:42:27 +0900

    Update Programmers Code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/정리중/[Lv.0] 배열 뒤집기/[Lv.0] 배열 뒤집기_00.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/정리중/[Lv.0] 배열 뒤집기/[Lv.0] 배열 뒤집기_00.java
@@ -3,7 +3,7 @@ class Solution {
         int[] answer = new int[num_list.length];
 
         for (int i = 0; i < num_list.length; i++) {
-            // num_list 배열의 마지막 원소부터 첫 번째 원소까지 역순으로 넣기
+            // 배열의 마지막 원소부터 첫 번째 원소까지 역순으로 넣기
             // 배열의 마지막 원소 = 배열의 길이 - 1
             answer[i] = num_list[num_list.length - 1 - i];
         }
--- Programmers/코딩테스트 입문/Java/Lv.0/정리중/[Lv.0] 배열 자르기/[Lv.0] 배열 자르기_02.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/정리중/[Lv.0] 배열 자르기/[Lv.0] 배열 자르기_02.java
@@ -5,6 +5,7 @@ class Solution {
         // 기본형 스트림 생성 -> 원소 매핑 -> 배열 변환
         // rangeClosed(start, end): start(포함)부터 end(포함)까지 범위 내 정수를 순서대로 생성
         // map(): 다른 형태의 원소로 매핑하여 반환
+        // toArray(): 배열 변환
         return IntStream.rangeClosed(num1, num2).map(i -> numbers[i]).toArray();
     }
 }
