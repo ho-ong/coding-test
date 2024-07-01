b9f0df2533f9c1572b3003b6cf3499cc6d7f2c73
ho-ong <honghj8959@gmail.com>
2024-07-01 20:07:22 +0900

    Update Programmers Code (Java)

--- Programmers/코딩 기초 트레이닝/Java/정리예정/등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기_00.java
+++ Programmers/코딩 기초 트레이닝/Java/정리예정/등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기_00.java
@@ -4,11 +4,10 @@ class Solution {
         // 공차: 등차수열이나 등차급수에서 서로 이웃하는 두 항의 차
         int answer = 0;
 
+        // 등차수열 구하기
         for (int i = 0; i < included.length; i++) {
-            // included 배열의 원소가 true일 경우
+            // 배열의 원소가 true일 경우
             if (included[i]) {
-                // 등차수열을 더하기
-                // 공차가 d이므로 d에 i를 곱하기
                 answer += a + (d * i);
             }
         }
--- Programmers/코딩 기초 트레이닝/Java/정리예정/등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기_01.java
+++ Programmers/코딩 기초 트레이닝/Java/정리예정/등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기_01.java
@@ -6,15 +6,12 @@ class Solution {
         // 공차: 등차수열이나 등차급수에서 서로 이웃하는 두 항의 차
 
         // 기본형 스트림 생성 -> 원소 매핑 -> 합계 계산
-        // 기본형 스트림: IntStream, LongStream, DoubleStream (숫자 스트림을 효율적으로 처리)
         // range(start, end): start(포함)부터 end(불포함)까지 범위 내 정수를 순서대로 생성
-        // map(): 스트림 내 기존 원소를 새로운 원소로 매핑하여 반환
+        // map(): 다른 형태의 원소로 매핑하여 반환
         // sum(): 합계 반환
 
-        // 0(포함)부터 included 배열의 길이(불포함)까지 범위 내 정수를 순서대로 생성
-        // included 배열의 원소가 true일 경우
-        // 등차수열의 합계를 구하기
-        // 공차가 d이므로 d에 i를 곱하기
+        // 등차수열 구하기
+        // 배열의 원소가 true일 경우
         return IntStream.range(0, included.length).map(i -> included[i] ? a + (d * i) : 0).sum();
     }
 }
